dd9c73d3b40179d203f2ca7c5d21177ef8364169
Paul Lammertsma <pflammertsma@gmail.com>
2016-07-06 16:49:13 +0300

    added account preferences added dummy screen for customizing feed

---
+++ app/src/main/java/com/example/android/basicsyncadapter/FeedSettingsActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.basicsyncadapter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FeedSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed_settings);
+    }
+
+}
