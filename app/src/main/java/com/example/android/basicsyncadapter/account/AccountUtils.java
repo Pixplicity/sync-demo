51e43051d8dc7499476ffd8b0955a506a3c3c604
Paul Lammertsma <pflammertsma@gmail.com>
2017-02-24 13:49:46 +0000

    code style

--- app/src/main/java/com/example/android/basicsyncadapter/account/AccountUtils.java
+++ app/src/main/java/com/example/android/basicsyncadapter/account/AccountUtils.java
@@ -54,8 +54,7 @@ public class AccountUtils {
         //
         // This string should *not* be localized. If the user switches locale, we would not be
         // able to locate the old account, and may erroneously register multiple accounts.
-        final String accountName = ACCOUNT_NAME;
-        return new Account(accountName, ACCOUNT_TYPE);
+        return new Account(ACCOUNT_NAME, ACCOUNT_TYPE);
     }
 
     public static boolean addAccount(Context context, Account account, long pollFrequency) {
